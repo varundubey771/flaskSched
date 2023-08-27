ea39ca92bcbb6971551c076d77f9afd3a2458e0b
varundubey771 <49694579+varundubey771@users.noreply.github.com>
2023-08-28 00:36:22 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,6 +14,7 @@ import subprocess
 # config = configparser.ConfigParser()
 # config.read('config.ini')
 
+
 # async def writer(data):
 #     db = Prisma(auto_register=True)
 #     await db.connect()
